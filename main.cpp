d55187b5dc945cd1366d2cd726f45bac0939a2d4
momoran <haiyuns@qq.com>
2020-10-17 17:29:21 +0800

    cmake调试,git操作

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "mathCalculate.h"
+
+int main()
+{
+    std::cout << "the max of 3 and 4 is: " << ReturnMax(3,4) << std::endl;
+    std::cout << "the min of 1 and 7 is: " << ReturnMax(1,7) << std::endl;
+    return 0;
+}
