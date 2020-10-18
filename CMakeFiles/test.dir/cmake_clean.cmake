9ab79a600d786593da3aed4be3b90d73cfa46368
momoran <haiyuns@qq.com>
2020-10-18 10:49:00 +0800

    添加自动化脚本rebuild

---
+++ CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/main.cpp.obj"
+  "build/libtest.dll.a"
+  "build/test.exe"
+  "build/test.exe.manifest"
+  "build/test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
